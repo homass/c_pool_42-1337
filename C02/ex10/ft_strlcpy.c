5b9a89f4688942aee2163acb9269049c4ef083de
Houssame Ait hsain <hossame@inbox.ru>
2022-11-16 13:42:58 +0100

    Create ft_strlcpy.c

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 16:14:53 by hait-hsa          #+#    #+#             */
+/*   Updated: 2022/03/29 21:34:14 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	j;
+	unsigned int	i;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size -1)
+		{
+		dest[i] = src[i];
+		i++;
+		}
+		dest[i] = '\0';
+	}
+	return (j);
+}
