32881824d5a0eac794c5b170bdb88d8f4fcc52ca
Houssame Ait hsain <hossame@inbox.ru>
2022-11-19 18:51:14 +0100

    Create ft_recursive_power.c

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 16:45:12 by hait-hsa          #+#    #+#             */
+/*   Updated: 2022/04/05 16:46:53 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	rec;
+
+	rec = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
